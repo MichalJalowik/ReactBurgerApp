438229bf977373911499f6afb3c191200e2abe45
MJ <michaljalowik@gmail.com>
2020-12-06 11:06:34 +0100

    fiest component layout created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-
+import Layout from '../src/components/Layout/Layout'
 
 class App extends Component {
   render() {
     return (
       <div>
-       <h1>Almost before we knew</h1>
+        <h1>Almost before we knew</h1>
+        <Layout>Test</Layout>
       </div>
     );
   }
