364125dbea859d9ebf02fed99cf4dd3a2e1d9f58
MJ <michaljalowik@gmail.com>
2020-12-08 20:07:17 +0100

    136

--- src/components/Burger/BulidControls/BuildControls.js
+++ src/components/Burger/BulidControls/BuildControls.js
@@ -3,27 +3,33 @@ import classes from './BulidControls.css';
 import BulidControl from './BulidControl/BulidControl'
 
 const controls = [
-  {label: 'Salad', type: 'salad'},
-  {label: 'Bacon', type: 'bacon'},
-  {label: 'Chease', type: 'cheese'},
-  {label: 'Meat', type: 'meat'},
+  { label: 'Salad', type: 'salad' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Chease', type: 'cheese' },
+  { label: 'Meat', type: 'meat' },
 
 ];
 
 const bulidControls = (props) => (
 
-<div className={classes.BulidControls}>
-  <p>Current price: <strong>{props.price.toFixed(2)} </strong> </p>
-  {controls.map(ctrl => (
-<BulidControl 
-added={() => props.ingAdd(ctrl.type)}
-removed={() => props.ingRemove(ctrl.type)}
-disabled={props.disabled[ctrl.type]}
-type={ctrl.label}
-key={ctrl.label} 
-label={ctrl.label}/>
-  ))}
-</div>
+  <div className={classes.BulidControls}>
+    <p>Current price: <strong>{props.price.toFixed(2)} </strong> </p>
+    {controls.map(ctrl => (
+      <BulidControl
+        added={() => props.ingAdd(ctrl.type)}
+        removed={() => props.ingRemove(ctrl.type)}
+        disabled={props.disabled[ctrl.type]}
+        type={ctrl.label}
+        key={ctrl.label}
+        label={ctrl.label} />
+    ))}
+    <button 
+      className={classes.OrderButton}
+      disabled={!props.purchasable}
+      >ORDER NOW</button>
+
+
+  </div>
 
 );
 
--- src/containers/BurgerBulider/BurgerBuilder.js
+++ src/containers/BurgerBulider/BurgerBuilder.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger'
 import BulidControls from '../../components/Burger/BulidControls/BuildControls';
@@ -20,73 +20,89 @@ class BurgerBulider extends Component {
 
     state = {
         ingredients: {
-           salad: 0,
-           bacon: 0,
-           cheese: 0,
-           meat: 1 
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
         },
-        totalPrice: 4
+        totalPrice: 4,
+        purchasable: false
+
+    }
+
+    updatePuchaseState (ingredients) {
+        
+
+        const sum = Object.keys(ingredients).map(igKey => {
+            return ingredients[igKey]
+        })
+        .reduce((sum, el) => {
+            return sum + el;
+        }, 0);
+        this.setState({purchasable: sum > 0});
 
     }
 
 
-addIngredientHandler = (type) => {
-const oldCount = this.state.ingredients[type];
-const updatedCount = oldCount + 1;
-const updatedingredients = {
-    ...this.state.ingredients
-};
-updatedingredients[type] = updatedCount;
-const priceAddition = ING_Prices[type];
-const oldPrice = this.state.totalPrice;
-const newPrice = oldPrice + priceAddition;
-this.setState({totalPrice: newPrice, ingredients: updatedingredients})
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+        const updatedingredients = {
+            ...this.state.ingredients
+        };
+        updatedingredients[type] = updatedCount;
+        const priceAddition = ING_Prices[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+        this.setState({ totalPrice: newPrice, ingredients: updatedingredients })
+        this.updatePuchaseState(updatedingredients);
+
+    }
+
+    removeIngredientHandler = (type) => {
+
+        const oldCount = this.state.ingredients[type];
+        if (oldCount <= 0) {
+            return;
+        }
 
 
-}
+        const updatedCount = oldCount - 1;
+        const updatedingredients = {
+            ...this.state.ingredients
+        };
+        updatedingredients[type] = updatedCount;
+        const priceDeduction = ING_Prices[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice - priceDeduction;
+        this.setState({ totalPrice: newPrice, ingredients: updatedingredients })
+        this.updatePuchaseState(updatedingredients);
 
-removeIngredientHandler = (type) => {
 
-    const oldCount = this.state.ingredients[type];
-if(oldCount <= 0){
-    return;
-}
-
-
-    const updatedCount = oldCount - 1;
-    const updatedingredients = {
-        ...this.state.ingredients
-    };
-    updatedingredients[type] = updatedCount;
-    const priceDeduction = ING_Prices[type];
-    const oldPrice = this.state.totalPrice;
-    const newPrice = oldPrice - priceDeduction;
-    this.setState({totalPrice: newPrice, ingredients: updatedingredients})
-    
-
-}
+    }
 
 
 
-    render () {
+    render() {
         const disabledInfo = {
-...this.state.ingredients
+            ...this.state.ingredients
         };
 
-        for(let key in disabledInfo) {
-             disabledInfo[key] = disabledInfo[key] <= 0
+        for (let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <= 0
         }
 
 
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients}></Burger>
-                
+
                 <BulidControls
-                ingAdd={this.addIngredientHandler}
-                ingRemove={this.removeIngredientHandler}
-                disabled={disabledInfo}
-                price={this.state.totalPrice}
+                    ingAdd={this.addIngredientHandler}
+                    ingRemove={this.removeIngredientHandler}
+                    disabled={disabledInfo}
+                    price={this.state.totalPrice}
+                    purchasable={this.state.purchasable}
                 />
             </Aux>
         );
